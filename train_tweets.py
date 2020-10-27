b154a21a75a7861fc66b3f592e7ce9f985c9be02
Elizabeth Keleshian <ekeleshian@way2b1.com>
2020-10-26 17:51:33 -0700

    pep8 formating

--- train_tweets.py
+++ train_tweets.py
@@ -5,28 +5,41 @@ import os
 
 def get_args():
     parser = argparse.ArgumentParser(description='Training GPT2 on sample')
-    parser.add_argument('--model_name',metavar='MODEL', help='name of the model you want to download 124M,355M,774M,1558M', required=True)
-    parser.add_argument('--csv_file',metavar='PATH', help='path of the csv file to train with', required=True)
+    parser.add_argument('--model_name', metavar='MODEL',
+                        help='name of the model you want to download 124M,355M,774M,1558M', required=True)
+    parser.add_argument('--csv_file', metavar='PATH', help='path of the csv file to train with', required=True)
     parser.add_argument('--steps', help='how many steps you want to train for',type=int, required=True)
     parser.add_argument('--run_name', help='name of the run you want to save in checkpoints folder', required=True)
-    parser.add_argument('--models_dir',metavar='PATH', help='name of the directory where the model is saved ', default='./models')
-    parser.add_argument('--combine',metavar='CHARS' ,help='Concatenate files with <|endoftext|> separator into chunks of this minimum size',type=int ,default=50000)
-    parser.add_argument('--batch_size', metavar='SIZE',help='number of examples it will train on at a time ', default=1,type=int)
+    parser.add_argument('--models_dir', metavar='PATH', help='name of the directory where the model is saved ',
+                        default='./models')
+    parser.add_argument('--combine', metavar='CHARS',
+                        help='Concatenate files with <|endoftext|> separator into chunks of this minimum size', type=int,
+                        default=50000)
+    parser.add_argument('--batch_size', metavar='SIZE',
+                        help='number of examples it will train on at a time ', default=1, type=int)
     parser.add_argument('--learning_rate', metavar='LR', help='learning rate of the optimizer', default=0.0001,)
-    parser.add_argument('--accumulate_gradients', type=int, default=1, help='Accumulate gradients across N minibatches.')
-    parser.add_argument('--checkpoint_dir', metavar='PATH',default='./checkpoint', help='The directory to save checkpoints in')
-    parser.add_argument('--sample_every', type=int,metavar='N', default=100, help='After how many steps do you want to generate a sample')
-    parser.add_argument('--sample_length', type=int,metavar='TOKENS', default=1024, help='size of the generated sameple')
-    parser.add_argument('--sample_num', type=int,metavar='N', default=1, help='number of samples you want to generate')
+    parser.add_argument('--accumulate_gradients', type=int, default=1, help='Accumulate gradients across N minibatches')
+    parser.add_argument('--checkpoint_dir', metavar='PATH', default='./checkpoint',
+                        help='The directory to save checkpoints in')
+    parser.add_argument('--sample_every', type=int, metavar='N', default=100,
+                        help='After how many steps do you want to generate a sample')
+    parser.add_argument('--sample_length', type=int, metavar='TOKENS', default=1024,
+                        help='size of the generated sameple')
+    parser.add_argument('--sample_num', type=int, metavar='N', default=1,
+                        help='number of samples you want to generate')
     parser.add_argument('--multi_gpu', type=bool, default=False, help='if you have multiple GPUs')
     parser.add_argument('--save_every', type=int, default=1000, help='after how many steps should the model save')
     parser.add_argument('--print_every', type=int, default=1, help='how often should the model print progress')
-    parser.add_argument('--max_checkpoints', type=int, default=1, help='how many checkpoints can exists together in the directory')
-    parser.add_argument('--use_memory_saving_gradients', type=bool, default=False, help='Use gradient checkpointing to reduce vram usage')
-    parser.add_argument('--only_train_transformer_layers', type=bool, default=False, help='Restrict training to the transformer blocks')
+    parser.add_argument('--max_checkpoints', type=int, default=1,
+                        help='how many checkpoints can exists together in the directory')
+    parser.add_argument('--use_memory_saving_gradients', type=bool, default=False,
+                        help='Use gradient checkpointing to reduce vram usage')
+    parser.add_argument('--only_train_transformer_layers', type=bool, default=False,
+                        help='Restrict training to the transformer blocks')
     parser.add_argument('--optimizer', default="adam", help='Optimizer to use while training defauls to adam')
-    parser.add_argument('--overwrite', default=False,type=bool, help='Overwrite the last run')
-    parser.add_argument('--restore_from', type=str, default='latest', help='Either "latest", "fresh", or a path to a checkpoint file')
+    parser.add_argument('--overwrite', default=False, type=bool, help='Overwrite the last run')
+    parser.add_argument('--restore_from', type=str, default='latest',
+                        help='Either "latest", "fresh", or a path to a checkpoint file')
     return parser.parse_args()
 
 
@@ -35,7 +48,7 @@ def download_model(args):
         print(f"Downloading {args.model_name} model...")
         gpt2.download_gpt2(model_name=args.model_name)
     else:
-        print(f"Model {args.model_name}  already downloaded")
+        print(f"Model {args.model_name} already downloaded")
 
 
 if __name__ == '__main__':
